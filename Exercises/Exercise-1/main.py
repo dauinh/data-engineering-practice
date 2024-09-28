bea56948d3b60a2971cb028c9f4e79129d76d78f
linh <linh.ha.nguyen20@gmail.com>
2024-09-27 19:00:41 -0700

    step 3

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
-import requests
+import requests, zipfile, os, shutil
+from io import BytesIO
+from pathlib import Path
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,10 +12,35 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+DOWNLOAD_DIR = Path("./downloads")
 
 def main():
-    # your code here
-    pass
+    # 1. create the directory downloads if it doesn't exist
+    if os.path.exists(DOWNLOAD_DIR) == False:
+        os.mkdir(DOWNLOAD_DIR)
+
+    # 2. download the files one by one.
+    for url in download_uris:
+        r = requests.get(url)
+        # 3. split out the filename from the uri, so the file keeps its original filename.
+        filename = url.split("/")[-1][:-4]
+        try:
+            zfile= zipfile.ZipFile(BytesIO(r.content))
+            zfile.extractall(DOWNLOAD_DIR / filename)
+            print(f"downloading {filename}...")
+        except zipfile.BadZipFile:
+            print(f"failed to download {filename}")
+
+        # 4. Each file is a zip, extract the csv from the zip and delete the zip file.
+        try:
+            csv_file = filename + ".csv"
+            shutil.move(DOWNLOAD_DIR / filename / csv_file, DOWNLOAD_DIR / csv_file)
+            shutil.rmtree(DOWNLOAD_DIR / filename)
+        except FileNotFoundError:
+            print(f"{csv_file} doesn't exist")
+
+
+    print("hello world")
 
 
 if __name__ == "__main__":
